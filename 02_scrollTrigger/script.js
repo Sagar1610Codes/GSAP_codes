ed40b1172e2d0cbd37af04fac603f567c1e90bfb
Sagar1610Codes <sagarvaishnav1610@gmail.com>
2025-03-14 00:24:26 +0530

    Scroll trigger updated

--- 02_scrollTrigger/script.js
+++ 02_scrollTrigger/script.js
@@ -1,18 +1,35 @@
 gsap.from("#page1 .box",{
     scale: 0,
     rotate: 360,
-    delay:1,
-    duration: 2
+    scrollTrigger: {
+        trigger: "#page1 .box",
+        scroller: "body",
+        markers:true,
+    }
 })
 
-gsap.from("#page2 .box",{
+
+gsap.fromTo("#page2 h1",{
+    x: 0
+},{
+    x: () => -document.querySelector("#page2 h1").offsetWidth,  // i want to make it more responsive
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        markers: true,
+        start: "top 0%",
+        end: "top -150%",
+        scrub:2,
+        pin: true
+    }
+})
+
+gsap.from("#page3 .box",{
     scale: 0,
     rotate: 360,
-    delay:1,
-    duration: 2,
     scrollTrigger: {
-        trigger: "#page2 .box",
+        trigger: "#page3 .box",
         scroller: "body",
-        markers: true
+        markers:true
     }
 })
